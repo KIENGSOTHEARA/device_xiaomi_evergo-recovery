4f45edd7b6188164d5501abe0bb909cf25bb1ba8
chickendrop89 <chickendrop89@gmail.com>
2026-01-03 19:33:25 +0100

    Revert "vendorsetup.sh: load settings/themes earlier"
    * This causes weird things like the recovery process restarting itself
    
    This reverts commit 0913d9a503f9db33e5ce8e0d3b1ff616cd9f6c43.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -77,7 +77,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ];
 
     # Store settings at /data/recovery instead of internal storage
     # export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
-    export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
 
     # Use latest "magiskboot" binaries as this is a relatively new device
     export FOX_USE_UPDATED_MAGISKBOOT=1
