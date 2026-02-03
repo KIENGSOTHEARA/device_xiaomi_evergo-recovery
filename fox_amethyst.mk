220954d1c27572c5d81cc2317e357c88516e403c
chickendrop89 <chickendrop89@gmail.com>
2026-02-03 01:24:42 +0100

    fox_amethyst.mk: add a limit to custom splash image sizes
    * We are having ramdisk size issues
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- fox_amethyst.mk
+++ fox_amethyst.mk
@@ -32,6 +32,9 @@ OF_DEFAULT_KEYMASTER_VERSION := 4.1
 OF_AB_DEVICE_WITH_RECOVERY_PARTITION := 1
 OF_ENABLE_ALL_PARTITION_TOOLS := 1
 
+# Fix recovery issues caused by large splash images
+OF_SPLASH_MAX_SIZE = 2048
+
 # Ignore the loop block errors after flashing ZIPs
 OF_LOOP_DEVICE_ERRORS_TO_LOG := 1
 
